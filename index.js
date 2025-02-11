3b9f4dc6acadc14a985bddfeedb70c5ed7486fbe
Angel C <[170134244+Angelcoco1985@users.noreply.github.com]>
2025-02-11 12:32:01 -0800

    Add button to receive input of new grid count and Add function to clear grid and to implement new

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 const container = document.querySelector(".container");
+const button = document.querySelector(".button");
+
+button.addEventListener("click", () => {
+    let userInput = prompt("How many squares per side would you like your grid?");
+    if (userInput <= 100) {
+        clearGrid();
+        let k = userInput;
+        function layOutGrid() {
+            for (let i = 0; i < (k * k); i++) {
+                const divs = document.createElement("div");
+                divs.classList.add("divDesign");
+                container.appendChild(divs);
+            }
+        }
+        
+        layOutGrid();
+
+    }
+})
 
 function layOutGrid() {
     for (let i = 0; i < 256; i++) {
@@ -7,6 +26,14 @@ function layOutGrid() {
         container.appendChild(divs);
     }
 }
+
+function clearGrid() {
+    container.innerHTML = "";
+}
+
+// divs.addEventListener("mouseenter", () => {
+//     divs.style.backgroundColor = "green";
+// })
 
 layOutGrid();
 
